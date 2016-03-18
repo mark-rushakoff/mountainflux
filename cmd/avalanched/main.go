b19fc55557240eb37e4fdff53971264020f6433c
Mark Rushakoff <mark@influxdb.com>
2016-03-18 09:26:08 -0700

    Bugfixes and more comments in avalanched

--- cmd/avalanched/main.go
+++ cmd/avalanched/main.go
@@ -19,19 +19,29 @@ import (
 var (
 	logger = log.New(os.Stdout, "[avalanched] ", log.LstdFlags)
 
+	// One WaitGroup for the HTTP writers
 	workersWg sync.WaitGroup
-	statsWg   sync.WaitGroup
-	bufPool   = sync.Pool{
+
+	// Separate WaitGroup for the stats writer (so that it can flush the last set of stats when workers shut down)
+	statsWg sync.WaitGroup
+
+	bufPool = sync.Pool{
 		New: func() interface{} {
 			return bytes.NewBuffer(make([]byte, 0, 4*1024))
 		},
 	}
 
-	// Input scanner sends buffers over this channel, writer workers read from it
-	blocksChan chan *bytes.Buffer
+	// Input scanner sends buffers over this channel, writer workers read from it.
+	// Created at runtime with buffer size equivalent to number of workers.
+	batchChan chan *bytes.Buffer
 
-	quit      = make(chan struct{})
+	// Channel closed by input scanner when input EOF reached.
+	// Indicates to rest of application that it is time to shut down.
 	inputDone = make(chan struct{})
+
+	// Channel closed by shutdown function.
+	// Indicates to stat flushing function to do one last flush.
+	quit = make(chan struct{})
 
 	statMu  sync.Mutex
 	statBuf *bytes.Buffer = bufPool.Get().(*bytes.Buffer)
@@ -64,14 +74,16 @@ func main() {
 		logger.Fatalf("no stats database provided (use e.g. -statsdb=mydb)")
 	}
 
-	blocksChan = make(chan *bytes.Buffer, *numWorkers)
+	batchChan = make(chan *bytes.Buffer, *numWorkers)
 
+	// One goroutine to periodically flush stats.
 	statsWg.Add(1)
 	go recordStats(avalanche.NewHTTPWriter(avalanche.HTTPWriterConfig{
 		Host:     "http://" + *statsURL,
 		Database: *statsDatabase,
 	}))
 
+	// Start the requested number of workers to make write requests over HTTP.
 	c := avalanche.HTTPWriterConfig{
 		Host:     "http://" + *url,
 		Database: *database,
@@ -79,15 +91,20 @@ func main() {
 	workersWg.Add(*numWorkers)
 	for i := 0; i < *numWorkers; i++ {
 		w := avalanche.NewHTTPWriter(c)
-		go processBlocks([]byte(*statsKey), w, blocksChan)
+		go processBatches([]byte(*statsKey), w, batchChan)
 	}
 	logger.Println("Beginning writes to", c.Host)
 
+	// Read input on a separate goroutine.
+	// Synchronization unnecessary here - if workers are stopped, input scanner will eventually fill the batchChan and block.
 	go scan(*linesPerBatch)
 
+	// Wait for either ctrl-c or end of input
 	waitForInterrupt()
 }
 
+// scan reads one line at a time from stdin.
+// When the requested number of lines per batch is met, send a batch over batchChan for the workers to write.
 func scan(linesPerBatch int) {
 	buf := bufPool.Get().(*bytes.Buffer)
 
@@ -100,8 +117,9 @@ func scan(linesPerBatch int) {
 
 		n++
 		if n >= linesPerBatch {
-			blocksChan <- buf
+			batchChan <- buf
 			buf = bufPool.Get().(*bytes.Buffer)
+			n = 0
 		}
 	}
 
@@ -109,32 +127,43 @@ func scan(linesPerBatch int) {
 		logger.Fatalf("Error reading input: %s", err.Error())
 	}
 
+	// Closing inputDone signals to the application that we've read everything and can now shut down.
 	close(inputDone)
 }
 
-func processBlocks(statsKey []byte, w avalanche.LineProtocolWriter, blocksChan <-chan *bytes.Buffer) {
+// processBatches reads byte buffers from batchChan and writes them to the target server, while tracking stats on the write.
+func processBatches(statsKey []byte, w avalanche.LineProtocolWriter, batchChan <-chan *bytes.Buffer) {
+	// Fields to hold write stats.
 	latField := river.Int{Name: []byte("latNs")}
 	successField := river.Bool{Name: []byte("ok")}
 	payloadField := river.Int{Name: []byte("payloadBytes")}
 	fields := []river.Field{&latField, &successField, &payloadField}
 
-	for block := range blocksChan {
-		latNs, err := w.WriteLineProtocol(block.Bytes())
+	for batch := range batchChan {
+		// Write the batch.
+		latNs, err := w.WriteLineProtocol(batch.Bytes())
 		if err != nil {
 			logger.Printf("Error writing: %s\n", err.Error())
 		}
 
+		// Track stats for the batch.
 		statMu.Lock()
 		ts := time.Now().UnixNano()
 		latField.Value = latNs
 		successField.Value = err == nil
-		payloadField.Value = int64(block.Len())
+		payloadField.Value = int64(batch.Len())
 		river.WriteLine(statBuf, statsKey, fields, ts)
 		statMu.Unlock()
+
+		// Return the batch buffer to the pool.
+		batch.Reset()
+		bufPool.Put(batch)
 	}
 	workersWg.Done()
 }
 
+// recordStats periodically tries to flush stats to stats server.
+// Also flushes stats if application is shutting down.
 func recordStats(statsW avalanche.LineProtocolWriter) {
 	t := time.NewTicker(3 * time.Second)
 	for {
@@ -151,18 +180,33 @@ func recordStats(statsW avalanche.LineProtocolWriter) {
 	}
 }
 
+// Send stats to stats server, if there are any stats to write.
 func flushStats(statsW avalanche.LineProtocolWriter) {
-	statMu.Lock()
-	if statBuf.Len() > 0 {
-		if _, err := statsW.WriteLineProtocol(statBuf.Bytes()); err != nil {
-			logger.Printf("Error writing stats: %s", err.Error())
-		}
+	// Temporary buffer so we can save stats while workers record stats.
+	var buf *bytes.Buffer
 
-		statBuf.Reset()
+	statMu.Lock()
+	// If no stats to record, we're done here.
+	if statBuf.Len() == 0 {
+		statMu.Unlock()
+		return
 	}
+
+	// There are stats to record, so hold on to the previous buffer and make a new one.
+	buf = statBuf
+	statBuf = bufPool.Get().(*bytes.Buffer)
 	statMu.Unlock()
+
+	if _, err := statsW.WriteLineProtocol(buf.Bytes()); err != nil {
+		logger.Printf("Error writing stats: %s", err.Error())
+	}
+
+	// Return the stats buffer to the pool.
+	buf.Reset()
+	bufPool.Put(buf)
 }
 
+// waitForInterrupt initiates shutdown on ctrl-c or input EOF.
 func waitForInterrupt() {
 	ctrlC := make(chan os.Signal, 1)
 	signal.Notify(ctrlC, os.Interrupt)
@@ -176,11 +220,13 @@ func waitForInterrupt() {
 	}
 }
 
+// shutdown signals to other goroutines to shut down.
+// If the other goroutines don't finish in time, forcefully shut down the application.
 func shutdown() {
 	done := make(chan struct{})
 
 	go func() {
-		close(blocksChan)
+		close(batchChan)
 		close(quit)
 		workersWg.Wait()
 		statsWg.Wait()
